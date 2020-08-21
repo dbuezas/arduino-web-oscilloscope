31c17ede0d0904519bf4365ceace01f6bddd8fa6
David Buezas <david.buezas@gmail.com>
2020-08-21 17:20:07 +0200

    vs just inline

--- arduino/src/fillBuffer.h
+++ arduino/src/fillBuffer.h
@@ -2,127 +2,103 @@
 
 uint8_t triggerVal;
 // uint16_t limit;
-#define storeOneMACRO(channel)                                       \
-  __attribute__((always_inline)) inline void storeOne_##channel() {  \
-    while (TCNT1 < state.ticksPerAdcRead) {                          \
-    }                                                                \
-    /* race condition here (what's with the 7 clocks?) */            \
-    TCNT1 -= state.ticksPerAdcRead - 7;                              \
-    uint8_t val0 = ADCH;                                             \
-    uint8_t val1 = (PINB & 0b00011111) | (PIND & 0b11100000);        \
-    uint8_t val2 = PINC & 0b00111100;                                \
-    if (channel == 0) triggerVal = val0;                             \
-    if (channel == 1) triggerVal = val1;                             \
-    if (channel > 1) triggerVal = val2 & (1 << channel);             \
-    buffer0[state.bufferStartPtr] = val0;                            \
-    buffer1[state.bufferStartPtr] = val1;                            \
-    buffer2[state.bufferStartPtr] = val2;                            \
-    /* masking against 512-1 so max is 512 == MAX_SAMPLES */         \
-    state.bufferStartPtr = (state.bufferStartPtr + 1) & 0b111111111; \
+
+__attribute__((always_inline)) byte storeOne(byte channel) {
+  while (TCNT1 < state.ticksPerAdcRead) {
+  } /* race condition here (what's with the 7 clocks?) */
+  TCNT1 -= state.ticksPerAdcRead - 7;
+  uint8_t val0 = ADCH;
+  uint8_t val1 = (PINB & 0b00011111) | (PIND & 0b11100000);
+  uint8_t val2 = PINC & 0b00111100;
+  buffer0[state.bufferStartPtr] = val0;
+  buffer1[state.bufferStartPtr] = val1;
+  buffer2[state.bufferStartPtr] =
+      val2; /* masking against 512-1 so max is 512 == MAX_SAMPLES */
+  state.bufferStartPtr = (state.bufferStartPtr + 1) & 0b111111111;
+  if (channel == 0) return val0;
+  if (channel == 1) return val1;
+  if (channel > 1) return val2 & (1 << channel);
+}
+
+__attribute__((always_inline)) void fillBufferAnalog(uint8_t channel,
+                                                     TriggerDir dir) {
+  uint8_t triggerPoint = state.triggerVoltage;
+  byte triggerVoltageMinus = max(0, (int)triggerPoint - 2);
+  byte triggerVoltagePlus = min(255, (int)triggerPoint + 2);
+  uint16_t headSamples = state.triggerPos;
+  uint16_t tailSamples = state.samplesPerBuffer - state.triggerPos;
+  startADC();
+  TCNT1 = 0;
+  while (headSamples--) {
+    storeOne(channel);
   }
-
-#define fillBufferMACROAnalog(channel, dir)                           \
-  void fillBuffer_##channel##_##dir() {                               \
-    uint8_t triggerPoint = state.triggerVoltage;                      \
-    byte triggerVoltageMinus = max(0, (int)triggerPoint - 2);         \
-    byte triggerVoltagePlus = min(255, (int)triggerPoint + 2);        \
-    uint16_t headSamples = state.triggerPos;                          \
-    uint16_t tailSamples = state.samplesPerBuffer - state.triggerPos; \
-    startADC();                                                       \
-    TCNT1 = 0;                                                        \
-    while (headSamples--) {                                           \
-      storeOne_##channel();                                           \
-    }                                                                 \
-    if (TriggerDir::dir == TriggerDir::rising) {                      \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal > triggerVoltageMinus);                     \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal < triggerPoint);                            \
-    } else {                                                          \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal < triggerVoltagePlus);                      \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal > triggerPoint);                            \
-    }                                                                 \
-    while (tailSamples--) {                                           \
-      storeOne_##channel();                                           \
-    }                                                                 \
-    stopADC();                                                        \
+  if (dir == TriggerDir::rising) {
+    while (storeOne(channel) > triggerVoltageMinus) {
+    }
+    while (storeOne(channel) < triggerPoint) {
+    }
+  } else {
+    while (storeOne(channel) < triggerVoltagePlus) {
+    }
+    while (storeOne(channel) > triggerPoint) {
+    }
   }
-#define fillBufferMACRODigital(channel, dir)                          \
-  void fillBuffer_##channel##_##dir() {                               \
-    uint16_t headSamples = state.triggerPos;                          \
-    uint16_t tailSamples = state.samplesPerBuffer - state.triggerPos; \
-    startADC();                                                       \
-    TCNT1 = 0;                                                        \
-    while (headSamples--) {                                           \
-      storeOne_##channel();                                           \
-    }                                                                 \
-    if (TriggerDir::dir == TriggerDir::rising) {                      \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal == 1);                                      \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal == 0);                                      \
-    } else {                                                          \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal == 0);                                      \
-      do {                                                            \
-        storeOne_##channel();                                         \
-      } while (triggerVal == 1);                                      \
-    }                                                                 \
-    while (tailSamples--) {                                           \
-      storeOne_##channel();                                           \
-    }                                                                 \
-    stopADC();                                                        \
+  while (tailSamples--) {
+    storeOne(channel);
   }
+  stopADC();
+}
 
-storeOneMACRO(0);
-storeOneMACRO(1);
-storeOneMACRO(2);
-storeOneMACRO(3);
-storeOneMACRO(4);
-storeOneMACRO(5);
-
-fillBufferMACROAnalog(0, rising);
-fillBufferMACROAnalog(1, rising);
-fillBufferMACRODigital(2, rising);
-fillBufferMACRODigital(3, rising);
-fillBufferMACRODigital(4, rising);
-fillBufferMACRODigital(5, rising);
-
-fillBufferMACROAnalog(0, falling);
-fillBufferMACROAnalog(1, falling);
-fillBufferMACRODigital(2, falling);
-fillBufferMACRODigital(3, falling);
-fillBufferMACRODigital(4, falling);
-fillBufferMACRODigital(5, falling);
-
-#define fillMacro(channel, dir)                                               \
-  if (state.triggerChannel == channel && state.triggerDir == TriggerDir::dir) \
-  fillBuffer_##channel##_##dir()
+__attribute__((always_inline)) inline void fillBufferDigital(uint8_t channel,
+                                                             TriggerDir dir) {
+  uint16_t headSamples = state.triggerPos;
+  uint16_t tailSamples = state.samplesPerBuffer - state.triggerPos;
+  startADC();
+  TCNT1 = 0;
+  while (headSamples--) {
+    storeOne(channel);
+  }
+  if (dir == TriggerDir::rising) {
+    while (storeOne(channel) == 1) {
+    }
+    while (storeOne(channel) == 0) {
+    }
+  } else {
+    while (storeOne(channel) == 0) {
+    }
+    while (storeOne(channel) == 1) {
+    }
+  }
+  while (tailSamples--) {
+    storeOne(channel);
+  }
+  stopADC();
+}
 
 void fillBuffer() {
-  // got it to 57 ticks
-  fillMacro(0, falling);
-  fillMacro(1, falling);
-  fillMacro(2, falling);
-  fillMacro(3, falling);
-  fillMacro(4, falling);
-  fillMacro(5, falling);
-
-  fillMacro(0, rising);
-  fillMacro(1, rising);
-  fillMacro(2, rising);
-  fillMacro(3, rising);
-  fillMacro(4, rising);
-  fillMacro(5, rising);
+  // got it to 57 ticks, now 55, now 58, now 56
+  uint8_t ch = state.triggerChannel;
+  TriggerDir d = state.triggerDir;
+  if (ch < 2)
+    fillBufferAnalog(ch, d);
+  else
+    fillBufferAnalog(ch, d);
+  return;
+  // if (d == TriggerDir::falling) {
+  //   if (ch == 0) fillBufferAnalog(ch, d);
+  //   if (ch == 1) fillBufferAnalog(ch, d);
+  //   if (ch == 2) fillBufferDigital(ch, d);
+  //   if (ch == 3) fillBufferDigital(ch, d);
+  //   if (ch == 4) fillBufferDigital(ch, d);
+  //   if (ch == 5) fillBufferDigital(ch, d);
+  // } else if (d == TriggerDir::rising) {
+  //   if (ch == 0) fillBufferAnalog(ch, d);
+  //   if (ch == 1) fillBufferAnalog(ch, d);
+  //   if (ch == 2) fillBufferDigital(ch, d);
+  //   if (ch == 3) fillBufferDigital(ch, d);
+  //   if (ch == 4) fillBufferDigital(ch, d);
+  //   if (ch == 5) fillBufferDigital(ch, d);
+  // }
 }
 
 // void fillBufferFast() {
