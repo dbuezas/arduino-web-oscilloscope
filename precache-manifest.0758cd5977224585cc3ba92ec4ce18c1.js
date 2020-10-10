c2aabfc1d22c338b5e28d552bbe273293777eb9c
dbuezas <dbuezas@users.noreply.github.com>
2020-10-10 21:39:18 +0000

    deploy: c9cf0cdc1de710f4724bee0a51918ba043330776

---
+++ precache-manifest.0758cd5977224585cc3ba92ec4ce18c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94abd123d6d21b1df5976cd3cb622ae7",
+    "url": "/arduino-web-oscilloscope/index.html"
+  },
+  {
+    "revision": "76cce03263cf8077f6c7",
+    "url": "/arduino-web-oscilloscope/static/css/2.7523cf1c.chunk.css"
+  },
+  {
+    "revision": "b23c1847d1847791b056",
+    "url": "/arduino-web-oscilloscope/static/css/main.d70b8e61.chunk.css"
+  },
+  {
+    "revision": "76cce03263cf8077f6c7",
+    "url": "/arduino-web-oscilloscope/static/js/2.084f153d.chunk.js"
+  },
+  {
+    "revision": "07efb4b0509eb13587b422a50ba3c61f",
+    "url": "/arduino-web-oscilloscope/static/js/2.084f153d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23c1847d1847791b056",
+    "url": "/arduino-web-oscilloscope/static/js/main.cd90cf46.chunk.js"
+  },
+  {
+    "revision": "6eb4fcf0ca8607e5c853",
+    "url": "/arduino-web-oscilloscope/static/js/runtime-main.3ac2590a.js"
+  }
+]);
