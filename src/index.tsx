f3a628716ad01c595616ee0645035f6425060ddb
David Buezas <david.buezas@gmail.com>
2020-10-11 02:48:02 +0200

    the zoom breaks dragging

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,6 @@ import App from './components/App'
 import * as serviceWorker from './serviceWorker'
 import { RecoilRoot } from 'recoil'
 
-if (process.env.NODE_ENV === 'production') {
-  // @TODO: app looks zomed in in development
-  // analysing the computed css, I can see that the
-  // HTML element has zoom=0.8 in dev and zoom=1 in production
-  document.documentElement.style.zoom = '0.8'
-}
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
